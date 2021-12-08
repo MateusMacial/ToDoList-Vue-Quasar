4a1f54e151bbe7988366add0f4ac321ec128521b
Mateus Macial <mateus_macial@hotmail.com>
2021-12-08 15:21:36 -0300

    Projeto Finalizado

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <q-field stack-label>
+
+      <div class="q-gutter-sm">
+        <q-checkbox v-model="val" @click="$emit('feito', index)" />
+      </div>
+
+      <div class="self-center full-width no-outline" tabindex="0">
+        <p>{{ tarefa }}</p>
+      </div>
+
+      <q-btn flat round color="primary" @click="$emit('deletar', index)" icon="delete" />
+    </q-field>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ToDoItem",
+  props: {
+    tarefa: { type: String },
+    realizado: { type: Boolean },
+    index: { type: Number },
+  },
+  data() {
+    return {
+      val: this.realizado,
+    };
+  },
+};
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <h2 class="subtitle">Inserir Tarefa</h2>
+    
+    <q-input outlined v-model.trim="tarefa.text" />
+
+    <q-btn color="primary" label="Add" @click="adicionarTarefa"/>
+
+    <h2 class="subtitle">Lista de Afazeres</h2>
+    <ToDoItem
+      v-for="(item, index) in lista"
+
+      :tarefa="item.text"
+      :realizado="item.realizada"
+      :index="index"
+      :key="item.text"
+      :class="{ tarefaFeita: item.realizada }"
+      @feito="feito"
+      @deletar="deletar"
+    >
+    </ToDoItem>
+  </div>
+</template>
+
+<script>
+import ToDoItem from '../components/ToDoItem.vue'
+
+export default {
+  components: { ToDoItem },
+  name: 'toDoList',
+  data () {
+    return {
+      lista: [],
+      tarefa: {
+        text: '',
+        realizada: false
+      }
+    }
+  },
+  methods: {
+    adicionarTarefa () {
+      let tarefa = {
+        text: this.tarefa.text,
+        realizada: false
+      }
+      this.lista.push(tarefa)
+      this.tarefa.text = ''
+    },
+    feito (index) {
+      var realizada = this.lista[index].realizada
+      if (realizada === true) {
+        this.lista[index].realizada = false
+      } else {
+        this.lista[index].realizada = true
+      }
+    },
+    deletar (index) {
+      this.lista.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+.lista {
+  list-style: none;
+}
+.tarefaFeita p {
+  text-decoration: line-through;
+}
+.subtitle{
+  font-family: 'Lucida Sans', 'Lucida Sans Regular';
+  padding-left: 20px;
+}
+</style>
